a5edd0f48537de5ceb86f89298a452f70a8585c0
Lígia kaylanne <142066909+ligiakaylanne@users.noreply.github.com>
2023-11-11 13:32:48 -0300

    atualizando

--- login/index.php
+++ login/index.php
@@ -1,4 +1,43 @@
-<!-- Seu arquivo PHP com HTML embutido -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="Provas/style2.css">
+</head>
+<body>
+    <form action="" method="POST">
+        <h1>Acesse sua conta</h1>
+
+        <?php if(isset($erro_email)) { ?>
+            <div class="error-message"><?php echo $erro_email; ?></div>
+        <?php } ?>
+
+        <?php if(isset($erro_senha)) { ?>
+            <div class="error-message"><?php echo $erro_senha; ?></div>
+        <?php } ?>
+
+        <?php if(isset($erro_login)) { ?>
+            <div class="error-message"><?php echo $erro_login; ?></div>
+        <?php } ?>
+
+        <p>
+            <label for="email">E-mail</label>
+            <input type="text" name="email" id="email">
+        </p>
+        <p>
+            <label for="senha">Senha</label>
+            <input type="password" name="senha" id="senha">
+        </p>
+        <p>
+            <button type="submit">Entrar</button>
+        </p>
+    </form>
+</body>
+</html>
 
 <?php
 include('conexao.php');
@@ -43,42 +82,3 @@ if(isset($_POST['email']) || isset($_POST['senha'])) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="Provas/style2.css">
-</head>
-<body>
-    <form action="" method="POST">
-        <h1>Acesse sua conta</h1>
-
-        <?php if(isset($erro_email)) { ?>
-            <div class="error-message"><?php echo $erro_email; ?></div>
-        <?php } ?>
-
-        <?php if(isset($erro_senha)) { ?>
-            <div class="error-message"><?php echo $erro_senha; ?></div>
-        <?php } ?>
-
-        <?php if(isset($erro_login)) { ?>
-            <div class="error-message"><?php echo $erro_login; ?></div>
-        <?php } ?>
-
-        <p>
-            <label for="email">E-mail</label>
-            <input type="text" name="email" id="email">
-        </p>
-        <p>
-            <label for="senha">Senha</label>
-            <input type="password" name="senha" id="senha">
-        </p>
-        <p>
-            <button type="submit">Entrar</button>
-        </p>
-    </form>
-</body>
-</html>
