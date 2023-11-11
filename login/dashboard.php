b6e6ab9da22b51b5a8545511f3933c18e46ad31d
Lígia kaylanne <142066909+ligiakaylanne@users.noreply.github.com>
2023-11-10 22:24:02 -0300

    atualizaçãoo

--- login/dashboard.php
+++ login/dashboard.php
@@ -23,6 +23,40 @@ if(empty($SESSION)){
                 print "Olá, ". $_SESSION["nome"];
                 print "<a href='logout.php' class='btn btn-danger'>Sair</a>"
                 ?>
+                <section id="download">
+        <a href="2022_PV_impresso_D1_CD1.pdf" download>Download ENEM impresso 2022(caderno azul)
+        </a>
+        <a href="2022_GB_impresso_D1_CD1(gabarito).pdf" download>Download Gabarito ENEM impresso 2022(caderno azul)
+        </a>
+        <a href="2021_PV_impresso_D1_CD1.pdf" download>Download ENEM impresso 2021(caderno azul)
+        </a>
+        <a href="2021_GB_impresso_D1_CD1.pdf" download>Download Gabarito ENEM impresso 2021(caderno azul)
+        </a>
+        <a href="2020_PV_impresso_D1_CD1.pdf" download>Download ENEM impresso 2020(caderno azul)
+        </a>
+        <a href="2020_GB_impresso_D1_CD1.pdf" download>Download Gabarito ENEM impresso 2020(caderno azul)
+        </a>
+        <a href="2019_PV_impresso_D1_CD1.pdf" download>Download ENEM impresso 2019(caderno azul)
+        </a>
+        <a href="gabarito_1_dia_caderno_1_azul_aplicacao_regular2019.pdf" download>Download Gabarito ENEM impresso 2019(caderno azul)
+        </a>
+        <a href="2018_PV_impresso_D1_CD1.pdf" download>Download ENEM impresso 2018(caderno azul)
+        </a>
+        <a href="GAB_ENEM_2018_DIA_1_AZUL.pdf" download>Download Gabarito ENEM impresso 2018(caderno azul)
+        </a>
+        <a href="2017_PV_impresso_D1_CD1.pdf" download>Download ENEM impresso 2017(caderno azul)
+        </a>
+        <a href="cad_1_gabarito_azul_511 2017.pdf" download>Download Gabarito ENEM impresso 2017(caderno azul)
+        </a>
+        <a href="2016_PV_impresso_D1_CD1.pdf" download>Download ENEM impresso 2016(caderno azul)
+        </a>
+        <a href="GAB_ENEM_2016_DIA_1_01_AZUL.pdf" download>Download Gabarito ENEM impresso 2016(caderno azul)
+        </a>
+        <a href="2015_PV_impresso_D1_CD1.pdf" download>Download ENEM impresso 2015(caderno azul)
+        </a>
+        <a href="CADERNO_1_AZUL_ 2015 gabarito.pdf" download>Download Gabarito ENEM impresso 2015(caderno azul)
+        </a>
+    </section>
         </div>
     </nav>
 </body>
--- login/logout.php
+++ login/logout.php
@@ -4,5 +4,5 @@
     unset($_SESSION["nome"]);
     unset($_SESSION["tipo"]);
     session_destroy();
-    header("Location: index.php");
+    header("Location: /index.html");
     exit;
